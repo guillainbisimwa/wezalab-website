4157dc11cde9b655f06d3a14c0552810f0b3cbf6
guillainbisimwa <guillainbisimwa@hotmail.fr>
2023-11-16 19:43:25 +0200

    update profile

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -11,9 +11,15 @@ const About = () => {
         </div>
         <div class="col-12 col-sm-12 col-lg-6">
           <h5>OUR COMPANY</h5>
-          <h2>Join us at <br /> WEZA LAB</h2>
-          <p> We are a Congolese start-up dedicated to fostering scientific research and collaboration in cutting-edge fields, including artificial intelligence (AI) and machine learning (ML). 
-            We are addressing local challenges, research and innovation, and social impact. </p>
+          {/* <h2>Join us at <br /> WEZA LAB</h2> */}
+          {/* <p> 
+            Weza Lab is a leading Congolese start-up bringing together technology, research, and social empowerment to create meaningful change in the Democratic Republic of the Congo.</p>
+            Weza Lab is more than just a technology start-up. We are a team of researchers, developers and specialists united by our shared passion to drive societal change and to contribute to sustainable development in the Democratic Republic of the Congo. */}
+
+<p>Weza Lab provides bespoke software solutions catered to distinctive community needs, addressing critical societal challenges.
+Our initiatives address a multiverse of sectors, including health, agriculture, climate action, connectivity, and open-source development.
+At Weza Lab, we believe in collaboration and inclusivity. Our multidisciplinary team comprises professionals passionate about creating social impact through technological advancement.
+Choosing Weza Lab means choosing a partner committed to fostering positive change, driving innovation, and prioritizing inclusivity and sustainability. Together, let's shape the future we aspire to. </p>
         </div>
       </div>
     </div>
