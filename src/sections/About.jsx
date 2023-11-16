b1f5588e7ced7037205b56f7a3e96daf09858f61
guillainbisimwa <guillainbisimwa@hotmail.fr>
2023-11-16 20:02:20 +0200

    Code refractor

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import about from '../assets/images/aboutimg.svg'
 
 const About = () => {
-    return <section class="about">
+  return <section class="about">
     <div class="container">
       <div class="row">
         <div class="col-12 col-sm-12 col-lg-6">
@@ -16,10 +16,10 @@ const About = () => {
             Weza Lab is a leading Congolese start-up bringing together technology, research, and social empowerment to create meaningful change in the Democratic Republic of the Congo.</p>
             Weza Lab is more than just a technology start-up. We are a team of researchers, developers and specialists united by our shared passion to drive societal change and to contribute to sustainable development in the Democratic Republic of the Congo. */}
 
-<p>Weza Lab provides bespoke software solutions catered to distinctive community needs, addressing critical societal challenges.
-Our initiatives address a multiverse of sectors, including health, agriculture, climate action, connectivity, and open-source development.
-At Weza Lab, we believe in collaboration and inclusivity. Our multidisciplinary team comprises professionals passionate about creating social impact through technological advancement.
-Choosing Weza Lab means choosing a partner committed to fostering positive change, driving innovation, and prioritizing inclusivity and sustainability. Together, let's shape the future we aspire to. </p>
+          <p>Weza Lab provides bespoke software solutions catered to distinctive community needs, addressing critical societal challenges.
+            Our initiatives address a multiverse of sectors, including health, agriculture, climate action, connectivity, and open-source development.
+            At Weza Lab, we believe in collaboration and inclusivity. Our multidisciplinary team comprises professionals passionate about creating social impact through technological advancement.
+            Choosing Weza Lab means choosing a partner committed to fostering positive change, driving innovation, and prioritizing inclusivity and sustainability. Together, let's shape the future we aspire to. </p>
         </div>
       </div>
     </div>
